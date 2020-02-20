48b1c250786300d1a6fd8a428d0871ddb198f785
Himanshu Pal <pal.himanshu1991@gmail.com>
2020-02-20 14:42:00 +0530

    Update Address.php

--- app/Models/Address.php
+++ app/Models/Address.php
@@ -4,12 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+use App\User;
+
 class Address extends Model
 {
-    protected $fillable = ['user_id', 'name', 'landmark', 'city', 'pincode', 'state', 'country'];
+    protected $fillable = [
+        'user_id',  // integer 
+        'name',     // varchar(50)
+        'landmark', // varchar(200)
+        'city',     // varchar(200)
+        'pincode',  // 
+        'state', 
+        'country'
+    ];
 
     public function user()
     {
-        return $this->belongsTo('App\User');
+        return $this->belongsTo(User::class);
     }
 }
